852626935e3d3319ff56c9b02aa012f1b9be5a2e
emdhruu <119742045+emdhruu@users.noreply.github.com>
2023-10-31 16:48:12 +0000

    non_primitive_datatype done

---
+++ 01_basics/05_datatype_summary.js
@@ -0,0 +1,16 @@
+//Non- primitive OR Reference type
+
+//Array
+const heros =["krish", "shaktiman", "harry-potter", "voldermort"];
+//Objects
+let myObj={
+    name:"dhruti",
+    age:"22"
+}
+//Functions
+const myFunction = function(){
+    console.log("Hello world!");
+}
+console.log(typeof heros);
+console.log(typeof myFunction);
+console.log(typeof myObj);
