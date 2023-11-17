9f90c56212ed364004bb1e40c48fd3ec6fbf6b63
emdhruu <119742045+emdhruu@users.noreply.github.com>
2023-11-17 13:57:17 +0000

    functions

---
+++ 03_basics/14_functions.js
@@ -0,0 +1,24 @@
+// function sayMyName(){
+//     console.log("D");
+//     console.log("H");
+//     console.log("R");
+//     console.log("U");
+//     console.log("T");
+//     console.log("I");
+// };
+
+// sayMyName();
+
+// function addTwoNumbers(number1 , number2){
+//     console.log(number1 + number2);
+// }
+function addTwoNumbers(number1 , number2){
+    // console.log(number1 + number2);
+    // let result= number1 +number2;
+    // return(result);
+    // console.log(number1 + number2); // after  return not reachable statement
+    return number1 +number2;
+}
+console.log(addTwoNumbers(3,4));
+// let result = addTwoNumbers(3,4);
+// console.log("result is : ",result);
