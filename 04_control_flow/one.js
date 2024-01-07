4779a0fe0023f1f94185d4b518ca683aba5e0383
emdhruu <119742045+emdhruu@users.noreply.github.com>
2024-01-07 07:03:52 +0000

    if statement

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -35,6 +35,31 @@ if (balance > 500) console.log("test"); //implict scope
 
 const level = 10;
 if (level >5)  console.log("level one completed"),
-console.log("level is score");
-    
+console.log("level is scored "); //not prefered to write this type of code
 
+//multiple condition check
+if(balance < 500){
+    console.log("less than 500");
+}
+else if (balance< 750){
+    console.log("less than 750");
+}
+else if(balance < 900){
+    console.log("less than 900");
+}
+else{
+    console.log("less than 1200");
+}
+
+const userloggedIn = true;
+const debitCard = true;
+
+const loggedInFromGoogle = false;
+const loggedInFromEmail = true;
+
+if (userloggedIn && debitCard) {
+    console.log("Allow to access course");
+}
+if (loggedInFromGoogle || loggedInFromEmail){
+    console.log("User Logged in");
+}
