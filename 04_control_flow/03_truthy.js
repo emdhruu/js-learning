fbd85ca7dbb3b4c56f84cd15afe94fb63cc62f86
emdhruu <119742045+emdhruu@users.noreply.github.com>
2024-01-09 05:58:07 +0000

    truthy

--- 04_control_flow/03_truthy.js
+++ 04_control_flow/03_truthy.js
@@ -29,12 +29,19 @@ if (Object.keys(emptyObj).length === 0) {
 // Nullish Coalescing Operator (??): null undefined
 
 let val1, val2, val3;
-val1 = 5 ?? 10;
-val2 = null ?? 10;
-val3 = undefined ?? 30;
-val4 = null ?? 10 ?? 20;
+val1 = 5 ?? 10; //5
+val2 = null ?? 10;  //10
+val3 = undefined ?? 30;   //30
+val4 = null ?? 10 ?? 20;  //10
 
 console.log(val1);
 console.log(val2);
 console.log(val3);
 console.log(val4);
+
+// Terniary Operator
+
+//condition ? true : false;
+
+const iceTeaPrice = 100;
+iceTeaPrice >= 80 ? console.log("less than 80") : console.log("more than 80");
