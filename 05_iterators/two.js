9480882cb4580a6b6bd4cd87547d31b89a50b835
emdhruu <119742045+emdhruu@users.noreply.github.com>
2024-01-20 12:34:36 +0000

    while and do-while loop

---
+++ 05_iterators/two.js
@@ -0,0 +1,20 @@
+//while
+
+let myArray = ['flash' , 'batman' , 'superman'];
+let arr = 0;
+while (arr < myArray.length ) {
+    console.log(`value of myarray is ${myArray[arr]}`);
+    arr++;
+}
+
+//do while
+let score = 1;
+do {
+    console.log(`value is ${score}`);
+    score++;
+} while (score <=10);
+let level = 11;
+do {
+    console.log(`value is ${score}`);
+    level++;
+} while (level<=10);
